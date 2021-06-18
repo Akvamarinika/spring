6e2c46d925c41fb8304bc0b9fd2aa62c265ecbc2
Akvamarinika <rozespink@mail.ru>
2021-06-18 23:38:10 +0800

    Added JS pop-up form

---
+++ 12.1Web-app_ToDoList/src/main/resources/static/js/myscript.js
@@ -0,0 +1,23 @@
+function openForm() {
+    let btnAdd = document.getElementById('show-add-event-form');
+    let btnSave = document.getElementById('save-event');
+    let container = document.getElementById('to-do-list-form');
+    let darkBG = document.createElement('div');
+
+    btnAdd.onclick = () => {
+        darkBG.style.height = window.innerHeight + 'px';
+        darkBG.style.width = window.innerWidth + 'px';
+        darkBG.id = 'dark-bg';
+        document.body.prepend(darkBG);
+
+        container.style.top = window.innerHeight / 2 - 50 + 'px';
+        container.style.left = window.innerWidth / 2 - 100 + 'px';
+        container.style.display = 'block';
+    }
+
+    btnSave.onclick = () => {
+        container.style.display = 'none';
+    }
+}
+
+openForm();
