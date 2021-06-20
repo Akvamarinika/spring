54c168cbc99f46e2a1a2f189866d9f9a92e4c9de
Akvamarinika <rozespink@mail.ru>
2021-06-21 02:53:35 +0800

    Added POSTFormEvent()

--- 12.1Web-app_ToDoList/src/main/java/myproject/controllers/EventsController.java
+++ 12.1Web-app_ToDoList/src/main/java/myproject/controllers/EventsController.java
@@ -1,8 +1,22 @@
 package myproject.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import response.Event;
+
+import java.util.List;
 
 @RestController
 public class EventsController {
 
+    @GetMapping("/events/")
+    public List<Event> eventList(){
+        return null;
+    }
+
+    @PostMapping("/events/add")
+    public void addNewEvent(){
+
+    }
 }
