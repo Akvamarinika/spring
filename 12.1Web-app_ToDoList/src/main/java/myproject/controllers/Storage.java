87149ce7d8114a669cc637896ede214de71f0c0a
Akvamarinika <rozespink@mail.ru>
2021-07-30 03:50:13 +0800

    UPD: added Storage method getEvent()

--- 12.1Web-app_ToDoList/src/main/java/myproject/controllers/Storage.java
+++ 12.1Web-app_ToDoList/src/main/java/myproject/controllers/Storage.java
@@ -1,22 +1,34 @@
 package myproject.controllers;
-
-import response.Event;
+import myproject.my_exceptions.NotFoundEvent;
+import myproject.response.Event;
+import myproject.response.NullEvent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Storage {
-    private static List<Event> eventsList = new ArrayList<>();
+    private static Map<Integer, Event> events = new HashMap<>();
+    private static int currentId = 1;
 
     public static int addEvent(Event event){
         System.out.println(event.getDate());
-        int id = eventsList.size() + 1;
+        int id = currentId++;
         event.setId(id);
-        eventsList.add(event);
+        events.put(id, event);
         return id;
     }
 
-    public static List<Event> getEventsList() {
-        return eventsList;
+    public static Event getEvent(int id){
+        if (events.containsKey(id)){
+            return events.get(id);
+        }
+        return new NullEvent();
+        //throw new NotFoundEvent(id, "The requested event is not in the database!");
+    }
+
+    public static List<Event> getAllEvents() {
+        return new ArrayList<>(events.values());
     }
 }
