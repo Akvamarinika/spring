632bb3c77f092176138096a3f8522e7fc1fbda9a
Akvamarinika <rozespink@mail.ru>
2021-07-31 02:50:16 +0800

    Added file getAllEvents.js

---
+++ 12.1Web-app_ToDoList/src/main/resources/static/js/getAllEvents.js
@@ -0,0 +1,24 @@
+import appendEvent from "./appendEvent.js";
+
+function GETListEvents( callbackInsertAll) {
+    let  url = '/events/';
+    fetch(url)
+        .then((response) =>
+            response.json()
+        )
+        .then((data) => {
+            console.log( "data----",  data);
+            callbackInsertAll(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function insertAllEvents(eventsList) {
+    for (let eventObj of eventsList){
+        appendEvent(eventObj);
+    }
+}
+
+GETListEvents(insertAllEvents);
