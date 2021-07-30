dc54cd617e1a517bb7815b9508b42c171f8458b9
Akvamarinika <rozespink@mail.ru>
2021-07-31 02:33:47 +0800

    rename script

---
+++ 12.1Web-app_ToDoList/src/main/resources/static/js/mainScript.js
@@ -0,0 +1,105 @@
+import checkAndCreateObjectForm from './createObjectFromForm.js';
+function openForm() {
+    let btnAdd = document.getElementById('show-add-event-form');
+    let btnSave = document.getElementById('save-event');
+    let container = document.getElementById('to-do-list-form');
+    let darkBG = document.createElement('div');
+
+    btnAdd.onclick = () => {
+        darkBG.style.height = window.innerHeight + 'px';
+        darkBG.style.width = window.innerWidth + 'px';
+        darkBG.id = 'dark-bg';
+        document.body.prepend(darkBG);
+
+        container.style.top = window.innerHeight / 2 - 300 + 'px';
+        container.style.left = window.innerWidth / 2 - 250 + 'px';
+        container.style.display = 'block';
+    }
+
+   btnSave.onclick = (event) => {
+        let formObj = checkAndCreateObjectForm();
+        if (formObj){
+            POSTFormEvent(formObj);
+            container.style.display = 'none';
+            document.body.removeChild(darkBG);
+            document.getElementById('myForm').reset();
+        }
+        event.stopPropagation();
+    }
+
+   darkBG.onclick = () => {
+        container.style.display = 'none';
+        document.body.removeChild(darkBG);
+    }
+}
+
+
+function GETListEvents( callbackInsertAll) {
+    let  url = '/events/';
+    fetch(url)
+        .then((response) =>
+             response.json()
+        )
+        .then((data) => {
+            console.log( "data----",  data);
+            callbackInsertAll(data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function insertAllEvents(eventsList) {
+    for (let eventObj of eventsList){
+        appendEvent(eventObj);
+    }
+}
+
+function POSTFormEvent(eventObj) {
+    let url = '/events/';
+
+    fetch(url, {
+          method: 'POST',
+          headers:{
+              'Accept':'application/json',
+              'Content-Type':'application/json'
+          },
+          body: JSON.stringify(eventObj)
+      })
+        .then((response) => {
+            return response.json();
+          })
+        .then(data => {
+            let event = {};
+            event.id = data;
+            //console.log('id---', data);
+            for (let key in eventObj){
+                event[key] = eventObj[key];
+            }
+            appendEvent(event);
+        })
+    .catch((error) => {
+        console.log(error);
+    });
+
+}
+
+const appendEvent = (event) => {
+    let container = document.querySelector('#list');
+    let div = document.createElement('div');
+    let eventHtml = `<a href="#" class="event-link" data-id="${event.id}">${event.theme}</a> 
+                type: ${event.type} 
+                date: ${event.date} 
+                timeStart: ${event.timeStart} 
+                timeEnd: ${event.timeEnd}`;
+    div.innerHTML = `<div>  ${eventHtml} </div>`;
+    container.appendChild(div);
+}
+
+
+
+
+
+openForm();
+GETListEvents(insertAllEvents);
+
