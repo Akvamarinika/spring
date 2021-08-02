3332caa1c45eba8eaaa03ca9eee72c2836448f5e
Akvamarinika <rozespink@mail.ru>
2021-08-03 02:01:19 +0800

    UPD: check Date & Time fields

--- 12.1Web-app_ToDoList/src/main/resources/static/js/createObjectFromForm.js
+++ 12.1Web-app_ToDoList/src/main/resources/static/js/createObjectFromForm.js
@@ -1,4 +1,4 @@
-function checkFieldsPresence() {
+function checkFieldsValidation() {
     let myForm = document.querySelector('#myForm');
     myForm.addEventListener('submit',(ev) => {
         ev.preventDefault();
@@ -16,12 +16,44 @@ function checkFieldsPresence() {
             }
         }
 
+        let dateStartPlan = document.querySelector('#date_start');
+        let dateEndPlan = document.querySelector('#date_end');
+        let dateStartInMs = dateConverter(dateStartPlan);
+        let dateEndInMs = dateConverter(dateEndPlan);
+        let dateNow = new Date().setHours(0,0,0,0);
 
+        if (dateStartInMs < dateNow) {
+            let error = generateError("Date entered must be greater than or equal to today's date!");
+            dateStartPlan.parentElement.after(error);
+        }
+
+        if (dateStartInMs > dateEndInMs) {
+            let error = generateError("Start date is greater than End date!");
+            dateEndPlan.parentElement.after(error);
+        }
+
+        let timeStart = document.querySelector('#time_start');
+        let timeEnd = document.querySelector('#time_end');
+        let timeStartInMs = timeConverter(timeStart);
+        let timeEndInMs = timeConverter(timeEnd);
+        if (dateStartInMs === dateEndInMs && timeStartInMs >= timeEndInMs){
+            let error = generateError("Time start is greater than or equal Time end!");
+            timeStart.parentElement.after(error);
+        }
 
     });
 
 }
 
+let dateConverter = (date) => {
+    let dateArr = date.value.split('-');
+    return +new Date(dateArr[0], dateArr[1]-1, dateArr[2]).setHours(0,0,0,0);
+}
+
+let timeConverter = (time) => {
+    let timeArr = time.value.split(':');
+    return +new Date().setHours(timeArr[0],timeArr[1],0,0);
+}
 
 let generateError = function (textError) {
     let error = document.createElement("span");
@@ -31,5 +63,4 @@ let generateError = function (textError) {
 }
 
 
-
-export default checkFieldsPresence();
+export default checkFieldsValidation;
