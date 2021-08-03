31aac212a6fc978b5ee7af2e1f9905c3b7781ee1
Akvamarinika <rozespink@mail.ru>
2021-08-03 23:20:45 +0800

    UPD: added new fields

--- 12.1Web-app_ToDoList/src/main/java/myproject/response/Event.java
+++ 12.1Web-app_ToDoList/src/main/java/myproject/response/Event.java
@@ -7,10 +7,17 @@ public class Event {
     String theme;
     String type;
     String place;
-    String date;
+    String dateStartPlan;
+    String dateEndPlan;
     String timeStart;
     String timeEnd;
-    String comment;
+    String description;
+    String status;
+    String dateStartActual;
+    String dateEndActual;
+    String timeStartActual;
+    String timeEndActual;
+    String priority;
 
     public int getId() {
         return id;
@@ -44,12 +51,20 @@ public class Event {
         this.place = place;
     }
 
-    public String getDate() {
-        return date;
+    public String getDateStartPlan() {
+        return dateStartPlan;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setDateStartPlan(String dateStartPlan) {
+        this.dateStartPlan = dateStartPlan;
+    }
+
+    public String getDateEndPlan() {
+        return dateEndPlan;
+    }
+
+    public void setDateEndPlan(String dateEndPlan) {
+        this.dateEndPlan = dateEndPlan;
     }
 
     public String getTimeStart() {
@@ -68,12 +83,60 @@ public class Event {
         this.timeEnd = timeEnd;
     }
 
-    public String getComment() {
-        return comment;
+    public String getDescription() {
+        return description;
     }
 
-    public void setComment(String comment) {
-        this.comment = comment;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDateStartActual() {
+        return dateStartActual;
+    }
+
+    public void setDateStartActual(String dateStartActual) {
+        this.dateStartActual = dateStartActual;
+    }
+
+    public String getDateEndActual() {
+        return dateEndActual;
+    }
+
+    public void setDateEndActual(String dateEndActual) {
+        this.dateEndActual = dateEndActual;
+    }
+
+    public String getTimeStartActual() {
+        return timeStartActual;
+    }
+
+    public void setTimeStartActual(String timeStartActual) {
+        this.timeStartActual = timeStartActual;
+    }
+
+    public String getTimeEndActual() {
+        return timeEndActual;
+    }
+
+    public void setTimeEndActual(String timeEndActual) {
+        this.timeEndActual = timeEndActual;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
     }
 
     public boolean isNull() {
