61c0c4d2cff7e6daacafca58f8cd7a6860ecedef
Akvamarinika <rozespink@mail.ru>
2021-08-09 23:28:39 +0800

    UPD: add html-tags

--- 12.1Web-app_ToDoList/src/main/resources/static/js/appendEvent.js
+++ 12.1Web-app_ToDoList/src/main/resources/static/js/appendEvent.js
@@ -1,12 +1,18 @@
 const appendEvent = (event) => {
     let container = document.querySelector('#list');
     let div = document.createElement('div');
-    let eventHtml = `<a href="#" class="event-link" data-id="${event.id}">${event.theme}</a> 
-                type: ${event.type} 
-                date: ${event.date} 
-                timeStart: ${event.timeStart} 
-                timeEnd: ${event.timeEnd}`;
-    div.innerHTML = `<div>  ${eventHtml} </div>`;
+    div.className = 'event-block';
+    div.dataset.id = `${event.id}`;
+    let eventHtml = `<a href="#" class="event-block__link" data-id="${event.id}">${event.theme}</a> 
+               <span> type: ${event.type} </span>
+               <span> dateStartPlan: ${event.dateStartPlan} </span>
+               <span> dateEndPlan: ${event.dateEndPlan} </span>
+               <span> timeStart: ${event.timeStart} </span>
+               <span> timeEnd: ${event.timeEnd} </span>
+                <a href="#" class="event-block__edit-link">Edit</a>
+                <a href="#" class="event-block__remove-link">Remove</a>`;
+
+    div.innerHTML = `${eventHtml}`;
     container.appendChild(div);
 }
 
