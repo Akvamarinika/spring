7fec7edbb8a42213b800460fc6370abf6bac6bf7
Akvamarinika <rozespink@mail.ru>
2021-08-13 23:10:52 +0800

    result set

--- JDBC/src/main/java/Main.java
+++ JDBC/src/main/java/Main.java
@@ -1,21 +1,32 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
+import java.sql.*;
 public class Main {
-    public static void main(String[] args){
-        String url = "jdbc:postgres://localhost:5432/demo";
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/comp";
         String user = "postgres";
         String pass = "postgres";
-        Connection connection = null;
+        try(Connection connection = DBConnect.getConnection(url, user, pass);
+            Statement statement = connection.createStatement()){
+            String query = "SELECT * FROM sales";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                int prodId = resultSet.getInt("product_id");
+                int quantity = resultSet.getInt("quantity");
+                int client_id = resultSet.getInt("client_id");
+                int employee_id =  resultSet.getInt("employee_id");
+                Date date = resultSet.getDate("implementation_date");
+                System.out.println(new Sale(id, prodId, quantity, client_id, employee_id, date));
 
-        try{
-            connection = DriverManager.getConnection(url, user, pass);
-
-            connection.close();
-        } catch (Exception e){
-            e.printStackTrace();
+            }
+        } catch (SQLException ex){
+            ex.printStackTrace();
         }
+
+
+
+
+
 
     }
 }
