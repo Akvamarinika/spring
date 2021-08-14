cb74026cc97279c193b0e721cd2aa7a68ca9e2cd
Akvamarinika <rozespink@mail.ru>
2021-08-15 02:52:16 +0800

    HQL: delete from DB

--- hibernate/src/main/java/Main.java
+++ hibernate/src/main/java/Main.java
@@ -15,10 +15,10 @@ public class Main {
 
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
-           Employee employee = session.get(Employee.class, 6L);
-           employee.setSalary(1200);
+           Employee employee = session.get(Employee.class, 3L);
+           session.delete(employee);
 
-           session.createQuery("update Employee set salary=1800 where name='Ivan'").executeUpdate();
+           session.createQuery("delete Employee where name='Ivan'").executeUpdate();
 
 
             session.getTransaction().commit();
