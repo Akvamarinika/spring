9ffee7ba41d929632bc36231c1c40d47d640411f
Akvamarinika <rozespink@mail.ru>
2021-08-15 20:55:38 +0800

    relation One-To-One (uni-directional)

--- hibernate/src/main/java/relations/Example.java
+++ hibernate/src/main/java/relations/Example.java
@@ -1,7 +1,46 @@
 package relations;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import relations.entity.Detail;
+import relations.entity.Employee;
+
 public class Example {
     public static void main(String[] args) {
+        Session session = null;
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+
+        try {
+            session = sessionFactory.getCurrentSession();
+            //Employee employee = new Employee("Petr", "Kapustin", "IT", 1900);
+            //Detail detail = new Detail("Tomsk", "89251234444", "kapustin@gmail.com");
+           // employee.setDetailId(detail);
+
+            session.beginTransaction();
+            //session.save(employee);
+
+            Employee emp = session.get(Employee.class, 2L);
+            session.delete(emp);
+
+            session.getTransaction().commit();
+            System.out.println(emp);
+            System.out.println(emp.getDetailId());
+        } catch (Exception e){
+            if (session != null) {
+                session.beginTransaction().rollback();
+            }
+    } finally {
+           if (session != null) {
+               session.close();
+           }
+            sessionFactory.close();
+        }
 
     }
 }
