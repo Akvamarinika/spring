a497a7ad5df9cbd0bb057451e065ddf6b32c0f3a
Akvamarinika <rozespink@mail.ru>
2021-08-18 21:32:53 +0800

    init

---
+++ hibernate/src/main/java/one_to_many_uni_dir/entity/Department.java
@@ -0,0 +1,108 @@
+package one_to_many_uni_dir.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String departmentName;
+
+    @Column(name = "max_salary")
+    private int maxSalary;
+
+    @Column(name = "min_salary")
+    private int minSalary;
+
+    @OneToMany( cascade = CascadeType.ALL)
+    @JoinColumn(name = "department_id") // fk in DB
+    private List<Employee> listEmp;
+
+    public Department() {
+    }
+
+    public Department(String departmentName, int minSalary, int maxSalary) {
+        this.departmentName = departmentName;
+        this.maxSalary = maxSalary;
+        this.minSalary = minSalary;
+    }
+
+    public void addEmployeeToDepartment(Employee employee){
+        if (listEmp == null){
+            listEmp = new ArrayList<>();
+        }
+        listEmp.add(employee);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(int maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public int getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(int minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public List<Employee> getListEmp() {
+        return listEmp;
+    }
+
+    public void setListEmp(List<Employee> listEmp) {
+        this.listEmp = listEmp;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + departmentName + '\'' +
+                ", maxSalary=" + maxSalary +
+                ", minSalary=" + minSalary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Department)) return false;
+        Department that = (Department) obj;
+        return departmentName.equals(that.departmentName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentName);
+    }
+}
