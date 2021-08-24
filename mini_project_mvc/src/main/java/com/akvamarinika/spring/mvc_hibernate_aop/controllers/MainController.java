3a2fb9972406e47cecb13fae3e54b07a8e4e8530
Akvamarinika <rozespink@mail.ru>
2021-08-25 03:10:02 +0800

    added layer Service

--- mini_project_mvc/src/main/java/com/akvamarinika/spring/mvc_hibernate_aop/controllers/MainController.java
+++ mini_project_mvc/src/main/java/com/akvamarinika/spring/mvc_hibernate_aop/controllers/MainController.java
@@ -2,6 +2,7 @@ package com.akvamarinika.spring.mvc_hibernate_aop.controllers;
 
 import com.akvamarinika.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.akvamarinika.spring.mvc_hibernate_aop.entities.Employee;
+import com.akvamarinika.spring.mvc_hibernate_aop.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,16 +13,16 @@ import java.util.List;
 
 @Controller
 public class MainController {
-    EmployeeDAO employeeDAO;
+    EmployeeService employeeService;
 
     @Autowired
-    public MainController(EmployeeDAO employeeDAO){
-        this.employeeDAO = employeeDAO;
+    public MainController(EmployeeService employeeService){
+        this.employeeService = employeeService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
     private String getPageAllEmployees(Model model){
-        List<Employee> employeeList = employeeDAO.getAllEmployees();
+        List<Employee> employeeList = employeeService.getAllEmployees();
         model.addAttribute("empList", employeeList);
         return "view-all-employees";
     }
--- mini_project_mvc/src/main/java/com/akvamarinika/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ mini_project_mvc/src/main/java/com/akvamarinika/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -6,8 +6,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Repository
@@ -20,7 +18,6 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     }
 
     @Override
-    @Transactional
     public List<Employee> getAllEmployees() {
         Session session = sessionFactory.getCurrentSession();
         Query<Employee> query = session.createQuery("from Employee",  Employee.class);
