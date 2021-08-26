74a9a4727baae391ba9df7ab3c07db55b6e1b369
Akvamarinika <rozespink@mail.ru>
2021-08-27 02:55:06 +0800

    added DepartmentDAO

---
+++ mini_project_mvc/src/main/java/com/akvamarinika/spring/mvc_hibernate_aop/dao/DepartmentDAOImpl.java
@@ -0,0 +1,29 @@
+package com.akvamarinika.spring.mvc_hibernate_aop.dao;
+import com.akvamarinika.spring.mvc_hibernate_aop.dao.repositories.DepartmentDAO;
+import com.akvamarinika.spring.mvc_hibernate_aop.entities.Department;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class DepartmentDAOImpl implements DepartmentDAO {
+    SessionFactory sessionFactory;
+
+    public DepartmentDAOImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<Department> getAll() {
+        Session session = sessionFactory.getCurrentSession();
+        List<Department> departmentList = session.createQuery("SELECT d FROM Department d", Department.class).getResultList();
+        return departmentList;
+    }
+
+    @Override
+    public void save(Department item) {
+
+    }
+}
---
+++ mini_project_mvc/src/main/java/com/akvamarinika/spring/mvc_hibernate_aop/dao/repositories/DepartmentDAO.java
@@ -0,0 +1,5 @@
+package com.akvamarinika.spring.mvc_hibernate_aop.dao.repositories;
+import com.akvamarinika.spring.mvc_hibernate_aop.entities.Department;
+
+public interface DepartmentDAO extends StandardDAO<Department> {
+}
